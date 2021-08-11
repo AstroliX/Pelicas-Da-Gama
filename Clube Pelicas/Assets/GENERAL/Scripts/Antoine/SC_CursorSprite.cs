59caaa72460c88bd1af385299809e8d33c783562
Antoine MACHINET <a.machinet@rubika-edu.com>
2021-08-11 15:45:20 +0100

    gitflow-feature-stash: BuildFixGA5Bis

---
+++ Clube Pelicas/Assets/GENERAL/Scripts/Antoine/SC_CursorSprite.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SC_CursorSprite : MonoBehaviour
+{
+    private Vector2 CursorPosition;
+
+    private bool IsVisible;
+
+    private RectTransform MyRT;
+    private Image MyImage;
+
+    void Start()
+    {
+
+        MyRT = GetComponent<RectTransform>();
+        MyImage = GetComponent<Image>();
+        
+    }
+
+    void Update()
+    {
+
+        CursorPosition = (Input.mousePosition);
+        CursorPosition.x = CursorPosition.x + 60;
+        CursorPosition.y = CursorPosition.y + -50;
+
+        MyRT.anchoredPosition = CursorPosition;
+
+        IsVisible = Cursor.visible;
+
+        if(IsVisible == true)
+        {
+          MyImage.color = new Color32(255, 161, 73, 255);
+        }
+
+        else
+        {
+            MyImage.color = new Color32(255, 161, 73, 0);
+
+        }
+
+        Debug.Log(CursorPosition);
+
+    }
+}
